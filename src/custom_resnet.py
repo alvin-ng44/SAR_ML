2f078cd180ce1ec34ca5978c8acf3a74c6d3ede6
nzhihao <nzhihao@D25004364.dsonet.corp.root>
2026-02-10 17:44:07 +0800

    Custom Resnet Class - delta

---
+++ src/custom_resnet.py
@@ -0,0 +1,10 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms, models
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.utils.data import DataLoader
+import re
+import copy
+import torchvision
